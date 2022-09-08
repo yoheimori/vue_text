c7f7d50fc00ff24935c9e8a853145a9e8316882c
yohei <moritayouheida@gmail.com>
2022-09-08 14:08:53 +0900

    P68

---
+++ js/event.js
@@ -0,0 +1,8 @@
+Vue.createApp({
+  data() {
+    return {
+      pet: "いぬ",
+    };
+  },
+  methods: {},
+}).mount("#app");
---
+++ js/watcher.js
@@ -0,0 +1,21 @@
+Vue.createApp({
+  data() {
+    return {
+      name: "",
+      upperName: "",
+    };
+  },
+  created() {
+    this.delayFunc = _.debounce(this.getUpper, 2000);
+  },
+  watch: {
+    name(newValue, oldValue) {
+      this.delayFunc();
+    },
+  },
+  methods: {
+    getUpper() {
+      this.upperName = this.name.toUpperCase();
+    },
+  },
+}).mount("#app");
